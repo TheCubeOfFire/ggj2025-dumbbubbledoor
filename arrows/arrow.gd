30f0fe57b6b10bb81b06c451c1c1ce0f0d4c64d0
Jnius0 <rjnius@gmail.com>
2025-01-25 01:03:39 +0100

    added arrow and bubble interractions and physics

--- arrows/arrow.gd
+++ arrows/arrow.gd
@@ -2,12 +2,26 @@ class_name Arrow
 
 extends RigidBody3D
 
+## magnitude of the launch force
+@export var initial_force: float
+
+## launch direction of the arrow
+@export var initial_direction: Vector3
+
+## multiplier to the bubble explosion force when this is in the radius of an explosion without having caused it
+@export var bubble_force_multiplier: float
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    pass # Replace with function body.
+    apply_central_impulse(initial_force * initial_direction.normalized())
 
+func bounce_on_bubble(force: Vector3):
+    var speed = linear_velocity.length()
+    linear_velocity = Vector3.ZERO
+    angular_velocity = Vector3.ZERO
+    linear_velocity = speed * force
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-    pass
+func push_from_bubble(force: Vector3):
+    linear_velocity = Vector3.ZERO
+    angular_velocity = Vector3.ZERO
+    apply_central_impulse(bubble_force_multiplier * force)
--- bubbles/bubble.gd
+++ bubbles/bubble.gd
@@ -2,27 +2,53 @@ class_name Bubble
 
 extends RigidBody3D
 
-@export var push_force_multiplier: float
+## multiplier when an explosion from an other bubble pushes this bubble
+@export var bubble_push_force_multiplier: float
 
 @onready var _explosion_area_collision: CollisionShape3D = $ExplosionArea/CollisionShape3D
 
+@onready var _mesh: MeshInstance3D = $Mesh
+
+@onready var _bubble_collision: CollisionShape3D = $BubbleCollision
+
+var _arrow_touched: Node3D
+
+var _exploded: bool = false
+
 
 func _on_body_entered(body: Node) -> void:
-    if is_instance_of(body, Arrow): 
+    if is_instance_of(body, Arrow) && !_exploded: 
+        body.bounce_on_bubble(_compute_explosion_direction(body))
+        _arrow_touched = body
         _explosion_area_collision.set_deferred("disabled",false)
+        _mesh.visible = false
+        _exploded = true
+        _bubble_collision.set_deferred("disabled",true)
+        var _timer = get_tree().create_timer(0.1)
+        await _timer.timeout
+        _disappear()
 
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
     if is_instance_of(body, Bubble) && body != self:
-        var force:  Vector3
-        var position = self.global_position
-        var other_position = body.global_position
-        force.x = other_position.x - position.x
-        force.y = other_position.y - position.y
-        force.normalized()
-        force *= push_force_multiplier / position.distance_to(other_position)
+        var force = _compute_explosion_direction(body)
+        force *= bubble_push_force_multiplier / self.global_position.distance_to(body.global_position)
         body.push(force)
-
+    elif is_instance_of(body, Arrow) && body != _arrow_touched:
+        body.apply_central_impulse(_compute_explosion_direction(body))
+        
+func _compute_explosion_direction(body: Node3D) -> Vector3:
+    var force = Vector3(0,0,0)
+    var self_position = self.global_position
+    var other_position = body.global_position
+    force.x = other_position.x - self_position.x
+    force.y = other_position.y - self_position.y
+    force.normalized()
+    return force
 
 func push(force: Vector3):
     apply_central_impulse(force)
+
+func _disappear():
+    _explosion_area_collision.set_deferred("disabled",true)
+    queue_free()
