175ace9790ccacb5b213c911f1f706b28a96a945
Jnius0 <rjnius@gmail.com>
2025-01-25 17:32:23 +0100

    added bouncers

---
+++ obstacles/bouncer.gd
@@ -0,0 +1,14 @@
+extends StaticBody3D
+
+@export var bounce_force: float
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+    if is_instance_of(body, GeneralRigidbody):
+        var force = Vector3(0,0,0)
+        var self_position = self.global_position
+        var other_position = body.global_position
+        force.x = other_position.x - self_position.x
+        force.z = other_position.z - self_position.z
+        force.normalized()
+        body.apply_central_impulse(force * bounce_force)
