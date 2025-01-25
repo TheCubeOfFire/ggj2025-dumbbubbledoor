70107ecba72dcd2d914bb868fff56c2cfb8490fe
Jnius0 <rjnius@gmail.com>
2025-01-25 19:25:16 +0100

    added bubble spawners

---
+++ spawners/bubble_spawner.gd
@@ -0,0 +1,58 @@
+extends Node3D
+
+const BUBBLE: PackedScene = preload("res://bubbles/Bubble.tscn")
+
+@export_node_path("Node3D") var bubble_container_node_path := ^""
+
+## min launch force
+@export var min_force: float
+## max launch force
+@export var max_force: float
+
+## min bubble interval
+@export var min_interval: float
+## max bubble interval
+@export var max_interval: float
+
+## launch angle min
+@export var _initial_direction_min: Vector3
+## launch angle max
+@export var _initial_direction_max: Vector3
+
+@onready var _bubble_container_node := get_node(bubble_container_node_path) as Node3D
+
+var min_spawn_position: Vector3
+var max_spawn_position: Vector3
+
+@onready var timer: Timer = $Timer
+
+
+func _ready() -> void:
+    min_spawn_position = global_position - (scale/2)
+    max_spawn_position = global_position + (scale/2)
+    _reset_timer()
+
+func _gen_random_pos():
+    var x = randf_range(min_spawn_position.x, max_spawn_position.x)
+    var z = randf_range(min_spawn_position.z, max_spawn_position.z)
+    return Vector3(x, 0, z)
+    
+func _gen_random_angle():
+    var x = randf_range(_initial_direction_min.x, _initial_direction_max.x)
+    var z = randf_range(_initial_direction_min.z, _initial_direction_max.z)
+    return Vector3(x, 0, z)
+
+
+func _on_timer_timeout() -> void:
+    var new_projectile := BUBBLE.instantiate()
+    if is_instance_of(new_projectile, GeneralRigidbody):
+        new_projectile.initial_force = randf_range(min_force, max_force)
+        new_projectile.initial_direction = _gen_random_angle()
+    _bubble_container_node.add_child(new_projectile)
+    new_projectile.global_position = _gen_random_pos()
+    _reset_timer()
+
+
+func _reset_timer():
+    timer.wait_time = randf_range(min_interval, max_interval)
+    timer.start()
